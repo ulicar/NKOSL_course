d1f93f11560f7ce06dce3a8f7892c2a87670cb7a
jdomsic <jdomsic@reversinglabs.com>
2015-03-16 01:27:13 +0100

    modifications

--- lvm_store.sh
+++ lvm_store.sh
@@ -18,21 +18,28 @@ size=$(du -sk test_dir | sed -r 's/([0-9]+).*/\1/')
 echo "Directory $DIR has a size of $size kb"
 
 
-##### Make a logical container
+# Make a logical container
 $container="lvm_container"
 $vg_name="vg_"$container
+$mount_point="tmp_filesystem"
 
 # max filesizes 25MB
 msize=$(echo "25 * 1024 * 1024" | bc)
 # size of a appropriate containter
 csize=$(echo "($size * 1024) / $msize + 1" | bc)
-
+# Crate empty space
 dd if=/dev/zero of=$container bs=$msize count=$csize > /dev/null 
+# Create loop setup
+losetup /dev/loop0 $container 
+# Crete filesistem
+mkfs -t ext4 /dev/loop0 
+# Mount tmp filesystem
+mount /dev/loop0 $mount_point
 
-pvcreate $container
+# create N containers
+pvcreate $mount_point
 vgcreate $vgname
-lvcreate -l 100%FREE -n lvn0 $vgname
-mkfs -t ext3 /dev/lvm-disk/lvm0
+# lvcreate -l 100%FREE -n lvn0 $vgname
 
 
 

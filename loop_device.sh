7bbdef3f5f43554f1b2b727786f2d3185d4fb740
jdomsic <jdomsic@reversinglabs.com>
2015-03-17 22:15:22 +0100

    prevent execution on sourcing

--- loop_device.sh
+++ loop_device.sh
@@ -2,14 +2,20 @@
 
 # Needs superuser
 function create_loop_device {
-    next_loop_number=$(ls /dev | tac | grep -E loop[0-9]+ -m1 | sed 's/loop//')
-    if [ -z $next_loop_number ]
-    do
-        $next_loop_number=0 
-    done
-
+    curr_loop_number=$(ls /dev | tac | grep -E loop[0-9]+ | cut -d'p' -f2 | sort -rn | head -n 1)
+    if [ -z $curr_loop_number ]
+    then
+        $curr_loop_number=-1
+    fi
+    
+    next_loop_number=$(echo "$curr_loop_number + 1" | bc)
+    
     mknod -m640 /dev/loop$next_loop_number b 7 $next_loop_number || exit 2
     chown root:disk /dev/loop$next_loop_number || exit 2
+    echo "Created /dev/loop$next_loop_number "
 }
 
-create_loop_device()
+if [[ $_ = $0 ]]
+then
+	create_loop_device
+fi

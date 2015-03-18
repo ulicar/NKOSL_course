841ad3142c2330a24bfb421ab507660b0b76398d
Josip Domsic <josip.domsic@gmail.com>
2015-03-18 18:37:20 +0100

    *not needed

--- destroy_loop_devices.sh
+++ destroy_loop_devices.sh
@@ -6,7 +6,7 @@
 
 function destroy_loop_devices {
     device='/dev/loop'
-    count=$(ls /dev | tac | grep -E loop[0-9]+ | cut -d'p' -f2 | sort -rn | head -n 1)
+    count=$(ls /dev | grep -E loop[0-9]+ | cut -d'p' -f2 | sort -rn | head -n 1)
     for id in $(eval echo {0..$count}) 
     do
         losetup -d $device$id 2>/dev/null || (rm $device$id && echo "Deleted $device$id")

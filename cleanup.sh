5fdbd9b349b0e6f7b6c4319d80f1c9afeafaa20e
Josip Domsic <josip.domsic@gmail.com>
2015-03-21 16:11:04 +0100

    *better cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -2,9 +2,14 @@
 
 source destroy_loop_devices.sh
 
-umount /dev/nkosl/new_fs
+umount /mnt/sending
+umount /mnt/nkosl
+
 lvremove /dev/nkosl/new_fs
+lvremove /dev/sending/restored
+
 vgremove nkosl
+vgremove sending
 
 for i in {0..20}
 do
@@ -13,5 +18,5 @@ done
 
 destroy_loop_devices
 
-rmdir /mnt/nkosl /dev/sending
+rmdir /mnt/nkosl /dev/sending /mnt/sending /dev/nkosl
 rm new_disk*

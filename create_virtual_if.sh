551463baf6b1b39ae316391753f31c9ad7d62086
Josip Domsic <josip.domsic@gmail.com>
2015-03-23 21:40:19 +0100

    troubles by Fedora

--- create_virtual_if.sh
+++ create_virtual_if.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
 # Create virual interface
-device="etho0:0"
-interface='/etc/network/interfaces'
+device="enp4s0_1"
+interface='/etc/sysconfig/network-scripts/ifcfg-'$device
 touch $interface
 
-cat >> $interface << EOFi
+cat >> $interface << EOF
 # NKOSL virtual interface
-auto $device
-iface $device inet static
-name Ethernet alias LAN card
-address 172.16.92.64
-netmask 255.255.255.192
-network 172.16.0.0
-gateway 172.16.92.120
+HWADDR=54:BE:F7:72:32:83
+TYPE=Ethernet
+BOOTPROTO=none
+IPADDR=172.16.92.64
+PREFIX=26
+GATEWAY=172.16.92.120
+DNS1=8.8.8.8
+DEFROUTE=yes
+IPV4_FAILURE_FATAL=no
+IPV6INIT=yes
+IPV6_AUTOCONF=yes
+IPV6_DEFROUTE=yes
+IPV6_PEERDNS=yes
+IPV6_PEERROUTES=yes
+IPV6_FAILURE_FATAL=no
+NAME=enp4s0_1
+UUID=5b0219cf-87e2-4d5f-982f-586448500324
+ONBOOT=yes
 
 EOF
 
-# Block incoming data, except IIRC
-iptables -A INPUT -i $device -p tcp --dport 6667 -j ACCEPT
 
+# Block incoming data, except IIRC
+iptables-save > iptables.old
+iptables -I INPUT 7 -i $device -p tcp --dport 6667 -j ACCEPT
+iptables -I INPUT 8 -i $device -p all -j DROP
+
+# Create Routes
+ip route add 172.16.0.0/26 via 172.16.92.120
 
 # Start interface
-ifup $device
-
+nmcli connection up enp4s0_1
 
 # Log created info
 ifconfig      > ifconfig.log
